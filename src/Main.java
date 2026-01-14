3dfea2126a7759696f23525294ebe9a1bb8033e9
hoonneo <a01022580502@gmail.com>
2026-01-15 00:01:14 +0900

    Scanner까지 복습 완료

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 void main() {
-    System.out.println("hello world!!");
-    System.out.println("HI");
-
-    for(int i=0;i<10;i++){
-        System.out.println(i);
+    for (int i=1;i<6;i++){
+        for (int k=0;k<i;k++){
+            System.out.print('*');
+        }
+        System.out.println();
     }
 
 }
